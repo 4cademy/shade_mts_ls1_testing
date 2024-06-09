7bf166153f041987908b141b05611375ec13e384
Marcel Beyer <marcelbeyer412@gmx.de>
2024-06-09 17:13:10 +0200

    Implemented 3rd objective function

--- objective_functions.h
+++ objective_functions.h
@@ -7,8 +7,6 @@
 #ifndef WORK_OBJECTIVE_FUNCTIONS_H
 #define WORK_OBJECTIVE_FUNCTIONS_H
 
-float objective_function_1(volatile float solution[DIM]);
-float objective_function_2(volatile float solution[DIM]);
-float (*objective_function[2])(volatile float solution[DIM]);
+float (*objective_function[3])(volatile float solution[DIM]);
 
 #endif //WORK_OBJECTIVE_FUNCTIONS_H
--- settings.h
+++ settings.h
@@ -8,10 +8,10 @@
 #define POPSIZE 100
 #define H 100    // size of history for cr and f
 #define DIM 1000
-#define MIN (-5)      // lower bound of the search space ATTENTION: depending on the function
-#define MAX 5         // upper bound of the search space ATTENTION: depending on the function
+#define MIN (-100)      // lower bound of the search space ATTENTION: depending on the function
+#define MAX 100         // upper bound of the search space ATTENTION: depending on the function
 #define FUNCTION_EVALS 3000000
 #define STEP_SIZE 25000
-#define FUNC_NO 1       // start at 0 -> 0 = objective_function_1 from CEC2013 benchmark
+#define FUNC_NO 0       // start at 0 -> 0 = objective_function_1 from CEC2013 benchmark
 
 #endif //WORK_SETTINGS_H
